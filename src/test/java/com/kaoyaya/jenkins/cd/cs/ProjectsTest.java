95f2039a4c4c6ae453072ab7eb9e7ee959fe2569
joe <adevjoe@gmail.com>
2019-01-19 00:31:33 +0800

    Change: resource name

--- src/test/java/com/kaoyaya/jenkins/cd/cs/ProjectsTest.java
+++ src/test/java/com/kaoyaya/jenkins/cd/cs/ProjectsTest.java
@@ -15,7 +15,7 @@ public class ProjectsTest {
     @Test
     public void getProjectList() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        URL url = classLoader.getResource("testDate/cert.json");
+        URL url = classLoader.getResource("testData/cert.json");
         File file = new File(Objects.requireNonNull(url).getFile());
         String content = FileUtils.readFileToString(file, "UTF-8");
         Cert cert = JSON.parseObject(content, Cert.class);
